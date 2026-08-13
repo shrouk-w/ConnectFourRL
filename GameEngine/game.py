01372742ed217530bfceac697987a9dfcf6589c2
Kacper-Wierzba <kacper.wierzbaa@wp.pl>
2026-08-13 15:10:53 +0200

    initial commit

---
+++ GameEngine/game.py
@@ -0,0 +1,36 @@
+﻿import numpy as np
+
+
+class ConnectFour:
+
+    def __init__(self):
+        self.rows = 6
+        self.columns = 7
+
+        self.board = np.zeros((self.rows, self.columns), dtype=np.int8)
+
+        self.current_player = 1
+        self.winner = None
+        self.done = False
+        self.move_count = 0
+        self.last_move = None
+
+    def reset(self):
+        self.board = np.zeros((self.rows, self.columns), dtype=np.int8)
+
+        self.current_player = 1
+        self.winner = None
+        self.done = False
+        self.move_count = 0
+        self.last_move = None
+
+        return self.get_state()
+
+    def get_state(self):
+        return self.board.copy()
+
+game = ConnectFour()
+
+print(game.board)
+print("Current player:", game.current_player)
+print("Done:", game.done)
